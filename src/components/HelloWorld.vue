9f53e4c62cd2dbb756ba8b808c69836e45c0a792
Jack Koppa <hello@jackkoppa.com>
2018-12-09 18:09:31 -0500

    feature: Add working API call to energy usage endpoint Call endpoint from sample HelloWorld component Display basic data from endpoint in interface, using component 'data'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,44 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="city in cities" :key="city.slug">
+        {{ city.name }}
+        {{ city.state }}
+        {{ city.usage.commercial.num_establishments }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { getEnergyUsageByCityAndState } from "@/api";
+import { capitalizeFirstLetter } from "@/mixins/utilities";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return { cities: [] };
+  },
+  mounted() {
+    getEnergyUsageByCityAndState("WAshington", "DC").then(response => {
+      if (response.data != null) {
+        const { inputs, result } = response.data;
+        const lowerCaseCity = inputs.city.toLowerCase();
+        const upperCaseState = inputs.state_abbr.toUpperCase();
+        const capitalizedCity = capitalizeFirstLetter(lowerCaseCity);
+        const city = {
+          slug: `${lowerCaseCity}-${upperCaseState}`,
+          name: capitalizedCity,
+          state: upperCaseState,
+          usage: result[capitalizedCity]
+        };
+        this.cities.push(city);
+      }
+    });
   }
 };
 </script>
