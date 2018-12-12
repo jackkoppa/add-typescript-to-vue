19ae41f836018f484a7fb6ee335f5e4a1c60ee55
Jack Koppa <hello@jackkoppa.com>
2018-12-12 06:11:31 -0500

    style: Create SCSS files; set base font to monospace

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,20 +9,20 @@
 </template>
 
 <style lang="scss">
+// Import global styles once, at app level
+@import "@/styles/global.scss";
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $cBlueGray;
 }
 #nav {
-  padding: 30px;
+  padding: $spacer/2;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $cBlueGray;
     &.router-link-exact-active {
-      color: #42b983;
+      color: $cGreen;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <h1 class="home__header">Search for Greenhouse Gas Emissions by City</h1>
     <Search/>
   </div>
 </template>
@@ -14,3 +15,12 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+@import "@/styles/_variables.scss";
+
+.home__header {
+  max-width: 75%;
+  margin: $spacer * 2 auto;
+}
+</style>
