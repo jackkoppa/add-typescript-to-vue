b0433b6e7c35b3e5604900dc67c337de8edac415
Jack Koppa <hello@jackkoppa.com>
2018-12-11 23:18:09 -0500

    docs: Add link to README in About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Check out the project README
+      <a href="https://github.com/jackkoppa/add-typescript-to-vue">here</a>
+    </h1>
   </div>
 </template>
